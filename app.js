d63aa5c5b78655253047fb6ac833c514c5aa7742
Nathan Stanford Sr <nathan.stanford@gmail.com>
2018-04-04 10:01:25 -0400

    Moved the css out of the html file and got the extra search boxes hidden to start.

--- app.js
+++ app.js
@@ -2,21 +2,17 @@ new Vue({
   el: '#app',
   data: {
     searchMode: false,
-    SearchParams: [
-      {  key: "name", name: "Name", placeholder: "Name..."}, 
-      {  key: "city", name: "City", placeholder: "City...", restrictToSuggestedValues: true, suggestedValues: ['Boston', 'New York','Dallas'] }, 
-      {  key: "country", name: "Cuntry", placeholder: "Country..."}, 
-      {  key: "emailAddress", name: "E-Mail", placeholder: "E-Mail..."}, 
-      {  key: "job", name: "Job", placeholder: "Job..."} 
-    ]
-  }
-  // ,
-  // template: `
-  // <div><h2>Search</h2>
-  //   Search: <input name="search" @keydown="searchMode = true">
-  //   <div v-if="searchMode">Parameter Suggestions: Name, City, Country, E-Mail, Job</div>
+    nameMode: false,
+    cityMode: false,
+    countryMode: false,
+    emailAddressMode: false,
+    jobMode: false,
+    searchValue:""
+  },
+  methods: {
+    checkSearch: function(event) {
+      this.searchValue = event.target.value;
+    }
 
-  //   <button>Search</button>
-  // </div>
-  // `
+  }
 })
