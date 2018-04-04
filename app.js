4dff0d414cee08e1ebd072b16e9bb91edda3ba9e
Nathan Stanford Sr <nathan.stanford@gmail.com>
2018-04-04 11:32:00 -0400

    Updating to not lose current changes this is not fully functional.

--- app.js
+++ app.js
@@ -10,8 +10,39 @@ new Vue({
     searchValue:""
   },
   methods: {
-    checkSearch: function(event) {
-      this.searchValue = event.target.value;
+    checkSearch: function() {
+      //alert("search Value = "+this.searchValue);
+      if(this.searchValue.toLowerCase() == 'name')
+      {
+        this.nameMode=true;
+        document.getElementById('name').focus();
+        this.test();
+      }
+      if(this.searchValue.toLowerCase() == 'city')
+      {
+        this.cityMode=true;
+        document.getElementById('city').focus();
+      }
+      if(this.searchValue.toLowerCase() == 'country')
+      {
+        this.countryMode=true;
+        document.getElementById('country').focus();
+      }
+      if(this.searchValue.toLowerCase() == 'e-mail')
+      {
+        this.emailAddressMode=true;
+        document.getElementById('emailAddress').focus();
+      }
+      if(this.searchValue.toLowerCase() == 'job')
+      {
+        this.jobMode=true;
+        document.getElementById('job').focus();
+      }
+
+    },
+    test: function() {
+      //alert('a');
+      this.searchValue ="";
     }
 
   }
